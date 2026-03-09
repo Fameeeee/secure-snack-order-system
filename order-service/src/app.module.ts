5b9346cb575cdf07d92414279a05fbdd65c53650
git1-bkksol <git1@bkksol.com>
2026-03-09 15:19:22 +0700

    feat: setup nest.js order-service with postgres connection

---
+++ order-service/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'process';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities:true,
+        synchronize:true,
+      }),
+      inject: [ConfigService],
+        })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
